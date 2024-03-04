0db521a69e075c31eb41851e060e1705a4d74cc6
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2024-03-04 06:38:20 -0700

    added environment.json and addEnvironment functions

--- PhaserHelpers.mjs
+++ PhaserHelpers.mjs
@@ -8,29 +8,63 @@ import {
     SquareMap
 } from './Map.mjs'
 
+
+
+/*** Preload Functions ***/
 function loadEnvironment(environment)
 {
+    const bg = environment.background
+    const layers = environment.layers
+    const decor = environment.decor
+    const props = environment.props
     let i = 1
-    this.load.image('background', environment.background.path)
-    environment.layers.forEach( layer => {
-        this.load.image(`layer${i}`, layer.path)
+
+    if(bg)
+    {
+        this.load.image('background', bg.path)
+    }
+    if(decor)
+    {
+        this.load.spritesheet(
+            decor.alias,
+            decor.path,
+            decor.frame)
+    }
+    if(props)
+    {
+        this.load.spritesheet(
+            props.alias,
+            props.path,
+            props.frame)
+    }
+    layers.forEach( layer => {
+        this.load.image(`layer${i+1}`, layer.path)
         i++
     })
-    this.load.spritesheet(
-        environment.tileset.alias,
-        environment.tileset.path,
-        environment.tilset.frame
-    )
-    this.load.spritesheet(
-        environment.props.alias,
-        environment.props.path,
-        environment.props.frame
-    )
 }
 
-function loadCharacter(character)
+function loadCharacterIdle(idle)
 {
-    this.load.spritesheet(character.idle.alias, character.idle.png, character.idle.frame)
+    this.load.spritesheet(idle.alias, idle.png, idle.frame)
+}
+
+/*** Create Functions ***/
+function addEnvironment(environment){
+    const bg = environment.background
+    const decor = environment.decor
+    const props = environment.props
+
+    if(bg){this.add.image(bg.width, bg.height, bg.alias)}
+    if(decor){this.addTileset(decor.alias, decor.path, decor.frame)}
+    if(props){this.addTileset(props.alias, props.path, props.frame)}
+    environment.layers.forEach(layer => {
+        this.add.image(layer.width, layer.height, layer.alias)
+    })
+}
+
+function addCharacterIdle(idle)
+{
+
 }
 
 export {
@@ -39,5 +73,6 @@ export {
     Map,
     SquareMap,
     loadEnvironment,
-    loadCharacter
+    loadCharacterIdle,
+    addEnvironment
 }
