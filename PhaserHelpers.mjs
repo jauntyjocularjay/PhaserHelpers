ec64c2f07bda06c85fab234611c2fca31a659882
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2024-03-03 22:57:59 -0700

    Added new json schema, helper functions

--- PhaserHelpers.mjs
+++ PhaserHelpers.mjs
@@ -8,9 +8,36 @@ import {
     SquareMap
 } from './Map.mjs'
 
+function loadEnvironment(environment)
+{
+    let i = 1
+    this.load.image('background', environment.background.path)
+    environment.layers.forEach( layer => {
+        this.load.image(`layer${i}`, layer.path)
+        i++
+    })
+    this.load.spritesheet(
+        environment.tileset.alias,
+        environment.tileset.path,
+        environment.tilset.frame
+    )
+    this.load.spritesheet(
+        environment.props.alias,
+        environment.props.path,
+        environment.props.frame
+    )
+}
+
+function loadCharacter(character)
+{
+    this.load.spritesheet(character.idle.alias, character.idle.png, character.idle.frame)
+}
+
 export {
     Frame,
     SquareFrame,
     Map,
-    SquareMap
+    SquareMap,
+    loadEnvironment,
+    loadCharacter
 }
