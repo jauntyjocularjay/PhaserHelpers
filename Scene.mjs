760b702913d2dd74130558ff29fe5ece8a9716dd
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2024-03-19 00:58:42 -0700

    commented out scene class

--- Scene.mjs
+++ Scene.mjs
@@ -1,41 +1,41 @@
-import { EA } from './Constants.mjs'
+// import { EA } from './Constants.mjs'
 
-/**
- * @summary
- *      this appears to be a problematic class
- *      phaser will not load with it
- */
+// /**
+//  * @summary
+//  *      this appears to be a problematic class
+//  *      phaser will not load with it
+//  */
 
-class Scene extends EA {
-    constructor(p, c, u)
-    {
-        super()
-        this.p = p
-        this.c = c
-        this.u = u
-    }
+// class Scene extends EA {
+//     constructor(p, c, u)
+//     {
+//         super()
+//         this.p = p
+//         this.c = c
+//         this.u = u
+//     }
 
-    preload(activeScene) {
-        this.p(activeScene)
-    }
+//     preload(activeScene) {
+//         this.p(activeScene)
+//     }
 
-    create(activeScene) {
-        this.c(activeScene)
-    }
+//     create(activeScene) {
+//         this.c(activeScene)
+//     }
 
-    update(activeScene) {
-        this.u(activeScene)
-    }
+//     update(activeScene) {
+//         this.u(activeScene)
+//     }
 
-    transform(activeScene){
-        return {
-            preload: this.p(activeScene),
-            create: this.c(activeScene),
-            update: this.u(activeScene)
-        }
-    }
-}
+//     transform(activeScene){
+//         return {
+//             preload: this.p(activeScene),
+//             create: this.c(activeScene),
+//             update: this.u(activeScene)
+//         }
+//     }
+// }
 
-export {
-    Scene
-}
+// export {
+//     Scene
+// }
